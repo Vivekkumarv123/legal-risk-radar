9401c39782bff7ff6a1c7d72b88560c9a41416f4
Vivekkumarv123 <164018649+Vivekkumarv123@users.noreply.github.com>
2026-01-03 16:56:58 +0530

    fix: mail secure transfer

--- src/utils/email.utils.js
+++ src/utils/email.utils.js
@@ -13,8 +13,8 @@ export const sendEmail = async ({ to, subject, html, text }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASS,
